93d21865583138d25775053206d9768d77b15df7
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2022-12-12 16:16:14 +0800

    Time: 526 ms (59.73%), Space: 15.4 MB (74.04%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -5,11 +5,11 @@ class Solution:
 
         while lower <= higher:
             mid = lower + (higher - lower) // 2
-            if target == nums[mid]:
-                return mid
             if target > nums[mid]:
                 lower = mid + 1
-            if target < nums[mid]:
+            elif target < nums[mid]:
                 higher = mid - 1
+            else:
+                return mid
 
         return -1
