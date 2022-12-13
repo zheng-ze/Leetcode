38375d93f79d2a0906614e8bf0022ce7b30ff84a
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2022-12-13 16:13:51 +0800

    Time: 138 ms (93.48%), Space: 14.8 MB (64.34%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -1,29 +1,12 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        mem = [[-1 for i in range(len(matrix))] for j in range(len(matrix))] 
-        print(mem)
+        for i in range(1, len(matrix)):
+            for j in range(len(matrix)):
+                if j == 0:
+                    matrix[i][j] += min(matrix[i-1][j], matrix[i-1][j+1])
+                elif j == len(matrix) - 1:
+                    matrix[i][j] += min(matrix[i-1][j], matrix[i-1][j-1])
+                else:
+                    matrix[i][j] += min(matrix[i-1][j], matrix[i-1][j-1], matrix[i-1][j+1])
         
-        def valid(arr, x, y):
-            if x < 0 or x >= len(arr) or y < 0 or y >= len(arr):
-                return False
-            return True
-        
-        def findMin(x, y, arr):
-            
-            if valid(arr, x, y):
-                if mem[x][y] != -1:
-                    return mem[x][y]
-                elif x == len(arr) - 1:
-                    mem[x][y] = arr[x][y]
-                    return arr[x][y]
-                mem[x][y] = arr[x][y] + min(findMin(x + 1, y, arr), findMin(x+1, y-1, arr), findMin(x+1, y+1, arr))
-                return mem[x][y]
-            else:
-                return 10000000
-        
-        minimum = 10000000
-        for i in range(len(matrix)):
-            temp = findMin(0, i, matrix)
-            minimum = min(minimum, temp)
-        
-        return minimum
+        return min(matrix[len(matrix) - 1])
