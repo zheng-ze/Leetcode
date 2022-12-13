823c5426c97422f7e2b45e8ad1201b6020d28a0f
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2022-12-13 16:42:52 +0800

    Time: 200 ms (12.43%), Space: 13.9 MB (90.23%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,25 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        direc = ((1, 0), (-1, 0), (0, 1), (0, -1))
+        orig = image[sr][sc]
+        queue = []
+        queue.append((sr, sc))
+        visited = [[False for j in range(len(image[0]))] for i in range(len(image))]
+        
+        while queue:
+            x, y = queue.pop(0)
+            if visited[x][y]:
+                continue
+            image[x][y] = color
+            visited[x][y] = True
+            for d in direc:
+                new_x = x + d[0]
+                new_y = y + d[1]
+                if new_x < 0 or new_x > len(image) - 1 or new_y < 0 or new_y > len(image[0]) - 1:
+                    continue
+                if visited[new_x][new_y]:
+                    continue
+                elif image[new_x][new_y] == orig:
+                    queue.append((new_x, new_y))
+                
+        return image
