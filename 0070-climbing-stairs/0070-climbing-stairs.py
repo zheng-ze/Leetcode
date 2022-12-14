50a670be9ab6fb642889eeab3a2e2b7312e3c6df
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2022-12-14 15:43:32 +0800

    Time: 42 ms (72.47%), Space: 13.9 MB (12.38%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        mem = {}
+        def climb(n):
+            if n <= 2:
+                return n
+            elif mem.get(str(n)):
+                return mem[str(n)]
+            else:
+                left = climb(n-1)
+                right = climb(n-2)
+                mem[str(n)] = left + right
+                return left + right
+        
+        return climb(n)
