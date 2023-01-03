e88f24f781e419871fda1795afe1ec999c697caa
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2023-01-03 17:36:43 +0800

    Time: 71 ms (5.14%), Space: 13.8 MB (65.79%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,19 +1,4 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        patterntos = {}
-        stopattern = {}
         list_s = s.split(" ")
-        if len(list_s) != len(pattern):
-            return False
-        for i in range(len(list_s)):
-            if pattern[i] not in patterntos:
-                patterntos[pattern[i]] = list_s[i]
-            elif patterntos[pattern[i]] != list_s[i]:
-                return False
-            if list_s[i] not in stopattern:
-                stopattern[list_s[i]] = pattern[i]
-            elif stopattern[list_s[i]] != pattern[i]:
-                return False
-        
-        return True
-            
+        return len(set(zip(pattern, list_s))) == len(set(pattern)) == len(set(list_s)) and len(list_s) == len(pattern)
