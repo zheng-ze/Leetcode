e52ff0f0016b0706780fe22b4b79be4522f673d1
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2023-01-03 17:37:01 +0800

    Time: 72 ms (5.14%), Space: 14 MB (16.23%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,4 +1,4 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         list_s = s.split(" ")
-        return len(set(zip(pattern, list_s))) == len(set(pattern)) == len(set(list_s)) and len(list_s) == len(pattern)
+        return len(set(zip(pattern, list_s))) == len(set(pattern)) == len(set(list_s)) and len(list_s) == len(pattern) 
