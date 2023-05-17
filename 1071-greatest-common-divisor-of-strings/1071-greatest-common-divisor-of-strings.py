3c0fd4e2e6203cdfa05bd11777de06b8136cf05a
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2023-05-17 17:20:24 +0800

    Time: 52 ms (27.87%), Space: 16.4 MB (17.66%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -2,8 +2,10 @@ import math
 
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
+        # Checks if there is a gcd
         if (str1 + str2 != str2 + str1):
             return ""
         
+        # Gets length of gcd and gets gcd
         lengcd = math.gcd(len(str1), len(str2))    
         return str1[:lengcd] 
