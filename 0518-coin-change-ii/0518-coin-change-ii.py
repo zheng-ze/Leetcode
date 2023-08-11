b02940e9e7f67395d44f9ea855f6a23ad3a323dd
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2023-08-11 09:29:42 +0800

    Time: 106 ms (99.05%), Space: 16.4 MB (97.28%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,11 +1,11 @@
 class Solution:        
     def change(self, amount: int, coins: List[int]) -> int:
-        self.dp = [0] * (amount + 1)
-        self.dp[0] = 1
+        dp = [0] * (amount + 1)
+        dp[0] = 1
         for coin in coins:
             for val in range(coin, amount + 1):
-                self.dp[val] += self.dp[val - coin]
+                dp[val] += dp[val - coin]
         
-        return self.dp[amount]
+        return dp[amount]
             
         
