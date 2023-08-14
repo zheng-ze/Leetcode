20208a93257e8fd63bac5e8ec70f97cfa4caaa72
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2023-08-14 09:59:47 +0800

    Time: 842 ms (93.80%), Space: 30.5 MB (62.79%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -6,10 +6,13 @@ class Solution:
         for i in range(2, len(nums)):
             valid = False
             
-            # If curr and prev are equal and if prev is valid, it is possible to partition valid subarrays
+            # If curr and prev are equal and if prev is valid, it is possible to partition valid subarrays 
+            # until i
             if not valid and dp[1] and nums[i] == nums[i - 1]:
                 valid = True
             
+            # If possible to form subarray of 3 and if possible to partition for prev, it is possible to
+            # partition subarrays until i
             if not valid and dp[0] and (nums[i] == nums[i - 1] == nums[i - 2] \
                                         or nums[i] == nums[i - 1] + 1 == nums[i - 2] + 2):
                 valid = True
