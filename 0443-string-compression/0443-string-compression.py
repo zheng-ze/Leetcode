e13abaf54480290facc8e960d14a171e8c0ea54b
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2023-08-15 09:50:25 +0800

    Time: 66 ms (81.68%), Space: 16.4 MB (81.87%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -6,20 +6,27 @@ class Solution:
         
         read, write = 0, 0
         
+        # Invariant: chars from index 0 to write is 'solved'
+        # Note: Since we find number of repeats using the inner loop, there is not need
+        # to track the previous character. Each iteration of outer loop will be for a new
+        # character
         while read < length:
             chars[write] = chars[read]
             count = 1
             write += 1
             
+            # Get num of repeats
             while read + 1 < length and chars[read] == chars[read + 1]:
                 read += 1
                 count += 1
             
+            # Write numbers if there are repeats
             if count > 1:
                 for char in str(count):
                     chars[write] = char
                     write += 1
             
+            # Move to next character
             read += 1
         
         return write
