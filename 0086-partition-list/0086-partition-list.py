1175e2034a6be62826e65a722d98590ee5651b34
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2023-08-15 09:22:40 +0800

    Time: 40 ms (88.16%), Space: 16.4 MB (14.86%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -11,17 +11,24 @@ class Solution:
         fill = ListNode(None, head)
         out = fill
         
+        
+        # fill will always point to outermost node that is smaller than x
+        # i.e the area that is "solved"
+        # Nodes between fill and head will be those that don't meet the criteria
         while head.next:
+            # current node is less than x -> new fill position
             if head.val < x:
-                fill = fill.next
+                fill = head
                 head = head.next
                 continue
+            # current node does not meet criteria but next node does -> swap
             if head.val >= x and head.next.val < x:
                 temp = head.next
                 head.next = head.next.next
                 temp.next = fill.next
                 fill.next = temp
                 fill = fill.next
+            # current node does not meet criteria but next node also doesn't -> move on to find one that does    
             else:
                 head = head.next
             
