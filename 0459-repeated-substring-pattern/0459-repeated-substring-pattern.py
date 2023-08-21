a572f8c7e3d3fa81c1ab53527fa346a4734cfc83
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2023-08-21 09:35:43 +0800

    Time: 49 ms (75.27%), Space: 16.3 MB (75.82%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -1,3 +1,11 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        return s in (s + s)[1 : -1]
+        # For all substring with length that can evenly divide,
+        # Check if repeating them to length of s makes them s.
+        # Do not check for substrings that cannot evenly divide as there is no way to become s.
+        for i in range(1, len(s) // 2 + 1):
+            if len(s) % i == 0:
+                if s[:i] * (len(s) // i) == s:
+                    return True
+        
+        return False
