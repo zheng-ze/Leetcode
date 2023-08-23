2633189a652245d3a5bfb14cd4b6db8c744202d1
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2023-08-23 09:47:58 +0800

    Time: 35 ms (93.57%), Space: 16.4 MB (46.15%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,25 +1,27 @@
+import heapq
+
 class Solution:
     def reorganizeString(self, s: str) -> str:
         counts = Counter(s)
         if counts.most_common(1)[0][1] >  (len(s) + 1) // 2:
             return ""
-        most_common = counts.most_common(1)[0]
-        out = [0] * len(s)
-        idx = 0
-        for i in range(most_common[1]):
-            out[idx] = most_common[0]
-            idx += 2
+        pq = [(-val, c) for (c, val) in counts.items()]
+        heapq.heapify(pq)
         
-        for (c, num) in counts.items():
-            if (c == most_common[0]):
-                continue
-            for i in range(num):
-                if idx >= len(s):
-                    idx = 1
-                
-                out[idx] = c
-                idx += 2
-                
+        prev_val, prev_c = 0, ""
+        out = [0 for i in range(len(s))]
+        idx = 0
+        
+        while pq:
+            if (prev_val < 0):
+                val, c = heapq.heapreplace(pq, (prev_val, prev_c))
+            else:
+                val, c = heapq.heappop(pq)
             
+            out[idx] = c
+            idx += 1
+            prev_val = val + 1
+            prev_c = c
+                
         return "".join(out)
         
