a104d1cbeeedbfcb4791f7d45b3b146def36c1c0
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2023-08-25 10:05:34 +0800

    Time: 38 ms (97.09%), Space: 17.5 MB (19.65%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -10,24 +10,21 @@ class Solution:
         
         # Idea to do BFS, find all possible combis until one of them reaches the end.
         q = deque([(0, 0)])
-        visited = set()
+        visited = set((0, 0))
         
         while q:
             idx1, idx2 = q.popleft()
-            
-            if (idx1, idx2) in visited:
-                continue
-            
-            visited.add((idx1, idx2))
-            
+                        
             if idx1 + idx2 == l3:
                 return True
             
-            if idx1 + 1 <= l1 and s1[idx1] == s3[idx1 + idx2]:
+            if idx1 < l1 and s1[idx1] == s3[idx1 + idx2] and (idx1 + 1, idx2) not in visited:
                 q.append((idx1 + 1, idx2))
+                visited.add((idx1 + 1, idx2))
             
-            if idx2 + 1 <= l2 and s2[idx2] == s3[idx1 + idx2]:
+            if idx2 < l2 and s2[idx2] == s3[idx1 + idx2] and (idx1, idx2 + 1) not in visited:
                 q.append((idx1, idx2 + 1))
+                visited.add((idx1, idx2 + 1))
 
         return False
                 
