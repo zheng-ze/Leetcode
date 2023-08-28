d4b4926423e9f497521ec25633f040b316b3d20e
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2023-08-28 09:46:14 +0800

    Time: 44 ms (40.26%), Space: 16.4 MB (67.63%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,17 +1,28 @@
 class MyStack:
-    # 
+    """
+    Idea to keep the front of the queue as the top of the stack.
+    Hence, we need to insert to the front of the queue.
+    However, we since, we are limited to the queue operations which only allows us to
+    insert to the back of the queue. Hence, we need to move all elements to the back
+    
+    Complexities:
+    Time = O(n) for insert, O(1) for everything else
+    Space = O(n)
+    """
     def __init__(self):
-        self.queue = []
+        self.queue = deque()
 
     def push(self, x: int) -> None:
-        self.queue.append(x) # offer
+        self.queue.append(x)
+        for i in range(len(self.queue) - 1):
+            self.queue.append(self.queue.popleft())
+        
         
     def pop(self) -> int:
-        return self.queue.pop() # poll
-        
+        return self.queue.popleft() # poll
 
     def top(self) -> int:
-        return self.queue[-1] # peek
+        return self.queue[0] # peek
         
 
     def empty(self) -> bool:
