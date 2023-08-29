b5613e40130379ecdc2f865f11055983b6561b8c
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2023-08-29 09:54:50 +0800

    Time: 239 ms (27.41%), Space: 25.6 MB (9.04%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
@@ -0,0 +1,30 @@
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        """
+        Idea to do 2 passes. 1 pass from the left to calculate the penalty incurred from shop being
+        open with no customers. Another pass from the right to calculate the penalty incurred from
+        shop being closed but customer come.
+        """
+        hours = len(customers)
+        openPenalty = [0 for i in range(hours + 1)]
+        closedPenalty = [0 for i in range(hours + 1)]
+        
+        penalty = 0
+        for hour in range(hours):
+            openPenalty[hour] = penalty
+            penalty += customers[hour] == 'N'
+        openPenalty[hours] = penalty
+        
+        penalty = 0
+        for hour in reversed(range(hours)):
+            closedPenalty[hour + 1] = penalty
+            penalty += customers[hour] == 'Y'
+        closedPenalty[0] = penalty
+        
+        penalty, idx = float('inf'), -1
+        for hour in range(len(openPenalty)):
+            if openPenalty[hour] + closedPenalty[hour] < penalty:
+                penalty = openPenalty[hour] + closedPenalty[hour]
+                idx = hour
+        return idx
+            
