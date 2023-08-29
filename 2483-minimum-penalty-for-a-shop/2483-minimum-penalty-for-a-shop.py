d013d863c0b32c2d2406d9eb4b60b9ebb4ef9d6f
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2023-08-29 10:02:38 +0800

    Time: 95 ms (96.39%), Space: 17.5 MB (46.99%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
@@ -1,31 +1,16 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
         """
-        Idea to do 2 passes. 1 pass from the left to calculate the penalty incurred from shop being
-        open with no customers. Another pass from the right to calculate the penalty incurred from
-        shop being closed but customer come.
+        Calculate the maximal profit for opening until that hour
         """
-        hours = len(customers)
-        openPenalty = [0 for i in range(hours + 1)]
-        closedPenalty = [0 for i in range(hours + 1)]
-        
-        penalty = 0
-        for hour in range(hours):
-            openPenalty[hour] = penalty
-            penalty += customers[hour] == 'N'
-        openPenalty[hours] = penalty
-        
-        penalty = 0
-        for hour in reversed(range(hours)):
-            closedPenalty[hour + 1] = penalty
-            penalty += customers[hour] == 'Y'
-        closedPenalty[0] = penalty
-        
-        lowest, idx = float('inf'), -1
-        for hour in range(len(openPenalty)):
-            penalty = openPenalty[hour] + closedPenalty[hour]
-            if penalty < lowest:
-                lowest = penalty
-                idx = hour
-        return idx
+        maxProfit = 0
+        profitHour = 0
+        currProfit = 0
+        for hour, customer in enumerate(customers):
+            currProfit += (1 if customer == 'Y' else -1)
             
+            if currProfit > maxProfit:
+                profitHour = hour + 1
+                maxProfit = currProfit
+        
+        return profitHour
