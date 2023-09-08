206d23e28b633b0383447d4f6a330ced9093288e
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2023-09-08 09:20:35 +0800

    Time: 43 ms (52.51%), Space: 16.4 MB (5.86%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,13 +1,16 @@
 class Solution:
     def __init__(self):
-        self.dp = [[1]]
+        self.dp = [[1]] # Memoise to reduce repeated computation
     def generate(self, numRows: int) -> List[List[int]]:
+        # If result is already memoised, just return it.
         if len(self.dp) >= numRows:
             return self.dp[:numRows]
         
+        # If previous row not computed yet, compute it first.
         if len(self.dp) < numRows - 1:
             self.generate(numRows - 1)
-
+        
+        # Compute current row based on previous row
         self.dp.append([1])
 
         for i in range(1, numRows - 1):
