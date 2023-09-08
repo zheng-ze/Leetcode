810e08f1fdfafe3bfb32d67a9863758bba5bfa73
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2023-09-08 09:19:14 +0800

    Time: 39 ms (75.33%), Space: 16.2 MB (71.54%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -8,13 +8,12 @@ class Solution:
         if len(self.dp) < numRows - 1:
             self.generate(numRows - 1)
 
-        row = [1]
+        self.dp.append([1])
 
         for i in range(1, numRows - 1):
-            row.append(sum(self.dp[numRows - 2][i - 1: i +1]))
+            self.dp[numRows - 1].append(sum(self.dp[numRows - 2][i - 1: i +1]))
         
-        row.append(1)
-        self.dp.append(row)
+        self.dp[numRows - 1].append(1)
         
         return self.dp
         
