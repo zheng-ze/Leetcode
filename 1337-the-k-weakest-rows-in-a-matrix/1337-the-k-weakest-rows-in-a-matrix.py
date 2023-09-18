7eb3b5dd6c3f6c6c1718a86e01290f4d6a2acc55
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2023-09-18 09:43:49 +0800

    Time: 101 ms (84.27%), Space: 16.6 MB (94.25%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -11,19 +11,11 @@ class Solution:
         
         # Keep the weakest k elements
         while i < len(mat):
-            print(weakest)
             if -sum(mat[i]) > weakest[0][0]:
                 heapq.heapreplace(weakest, (-sum(mat[i]), -i))
             i += 1
-        
-        # Sort by strength then by index
-        def cmp(x, y):
-            if x[0] == y[0]:
-                return y[1] - x[1]
-            return y[0] - x[0]
             
-        weakest = sorted(weakest, key=functools.cmp_to_key(cmp))
-        return [-row[1] for row in weakest]
+        return [-row[1] for row in sorted(weakest, key=lambda x: (-x[0], -x[1]))]
             
             
             
