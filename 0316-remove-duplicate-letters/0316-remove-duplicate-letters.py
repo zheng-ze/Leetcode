bc884ecdf82aaf39c57ba57bdb7dc447653e1702
Zheng Ze <84606153+pzhengze@users.noreply.github.com>
2023-09-26 10:22:23 +0800

    Time: 43 ms (55.82%), Space: 16.4 MB (18.14%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -2,20 +2,21 @@ class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         indices = dict()
         stack = []
+        length = len(s)
         used = set()
         
-        for i in range(len(s)):
+        for i in range(length):
             indices[s[i]] = i
                     
-        for i, c in enumerate(s):
-            if c in used:
+        for i in range(length):
+            if s[i] in used:
                 continue
             
             # Pop all elements that are smaller lexicographically and exist later on
-            while stack and c < stack[-1] and indices[stack[-1]] > i:
+            while stack and s[i] < stack[-1] and indices[stack[-1]] > i:
                 used.remove(stack.pop())
             
-            stack.append(c)
-            used.add(c)
+            stack.append(s[i])
+            used.add(s[i])
         
         return "".join(stack)
