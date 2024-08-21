a1a7b5a227f6fb98dbe338e473752ee266f9c436
Zheng Ze <zhengze@u.nus.edu>
2024-08-22 00:40:43 +0800

    Time: 39 ms (66.87%), Space: 16.5 MB (87.66%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -2,18 +2,12 @@ class Solution:
     def minSteps(self, n: int) -> int:
         if n <= 1:
             return 0
-                
-        def helper(savedLength: int, currLength: int, currOperations: int) -> int:
-            if currLength == n:
-                return 0
-            
-            if currLength > n:
-                return float('inf')
-            
-                        
-            if currOperations & 1 == 0:
-                return 1 + helper(currLength, currLength + savedLength, currOperations << 1 | 1)
-            
-            return 1 + min(helper(savedLength, currLength + savedLength, currOperations << 1 | 1), helper(currLength, currLength, currOperations << 1))
         
-        return 1 + helper(1, 1, 0)
+        result = 0
+        
+        for i in range(2, n + 1, 1):
+            while n % i == 0:
+                result += i # Need to paste i number of times
+                n /= i
+        
+        return result
