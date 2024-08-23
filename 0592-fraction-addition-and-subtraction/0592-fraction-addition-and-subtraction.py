cb643c0dae8507fee3fcbf9bca9bc664ad69e0c7
Zheng Ze <zhengze@u.nus.edu>
2024-08-24 02:17:38 +0800

    Time: 37 ms (48.84%), Space: 16.6 MB (26.36%) - LeetHub

---
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
@@ -0,0 +1,39 @@
+class Solution:
+    def fractionAddition(self, expression: str) -> str:
+        numerator = 0
+        denominator = 1
+        
+        i = 0
+        length = len(expression)
+        while i < length:
+            curr_numerator = 0
+            curr_denominator = 0
+            
+            sign = 1
+            
+            if expression[i] == '-':
+                sign = -1
+            
+            if not expression[i].isnumeric():
+                i += 1
+            
+            while i < length and expression[i].isnumeric():
+                curr_numerator = curr_numerator * 10 + int(expression[i])
+                i += 1
+        
+            i += 1
+            
+            while i < length and expression[i].isnumeric():
+                curr_denominator = curr_denominator * 10 + int(expression[i])
+                i += 1
+            
+            numerator = numerator * curr_denominator + curr_numerator * denominator * sign
+            denominator *= curr_denominator
+            
+        gcd = abs(math.gcd(numerator, denominator))
+        
+        numerator //= gcd
+        denominator //= gcd
+        
+        return f"{numerator}/{denominator}"
+                
