83d705a84a2d25ecd3b2fa3dd5a9d98b685fd0e4
Zheng Ze <zhengze@u.nus.edu>
2024-08-24 02:59:32 +0800

    Time: 32 ms (86.01%), Space: 16.4 MB (66.81%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -2,18 +2,11 @@ from collections import deque
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        removedPositions = deque([])
-        removed = 0
+        fillIndex = 0
+        
         for i in range(len(nums)):
-            if nums[i] == val:
-                removedPositions.append(i)
-                removed += 1
-                continue
-            
-            if len(removedPositions) > 0:
-                fillPos = removedPositions[0]
-                removedPositions.popleft()
-                nums[fillPos] = nums[i]
-                removedPositions.append(i)
-                
-        return len(nums) - removed
+            if nums[i] != val:
+                nums[fillIndex] = nums[i]
+                fillIndex += 1
+        
+        return fillIndex
