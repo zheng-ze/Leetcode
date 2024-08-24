9989380176e221805a5614e8907558583318d483
Zheng Ze <zhengze@u.nus.edu>
2024-08-24 23:16:54 +0800

    Time: 56 ms (29.07%), Space: 16.6 MB (48.18%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -11,13 +11,7 @@ class Solution:
             nums[pos] = nums[i]
             pos += 1
             
-            # Allows for at most 1 duplicate
-            if i < len(nums) - 1 and nums[i] == nums[i + 1]:
-                nums[pos] = nums[i]
-                i += 1
-                pos += 1
-            
-            while i < len(nums) - 1 and nums[i] == nums[i + 1]:
+            while i < len(nums) - 2 and nums[i] == nums[i + 2]:
                 i += 1
             
             i += 1
