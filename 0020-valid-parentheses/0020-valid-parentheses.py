2da27af88ec479b917e5ebb64defaefb949ba54b
Zheng Ze <zhengze@u.nus.edu>
2024-08-25 22:24:43 +0800

    Time: 38 ms (38.59%), Space: 16.6 MB (49.28%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,14 +1,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
+        dic = {"(": ")", "[": "]", "{": "}"}
         
         for c in s:
-            if c == '(':
-                stack.append(')')
-            elif c == '[':
-                stack.append(']')
-            elif c == '{':
-                stack.append('}')
+            if c in dic:
+                stack.append(dic[c])
             elif not stack or stack.pop(-1) != c:
                 return False
         
