79694bc792dbb7e100ac71fa63de9aa0fb98a8aa
Zheng Ze <zhengze@u.nus.edu>
2024-09-03 12:17:51 +0800

    Time: 379 ms (99.37%), Space: 21.2 MB (11.59%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,34 +1,51 @@
+from collections import defaultdict
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         length = len(nums)
         out = []
-        nums.sort()
+        positives, negatives, zeros = defaultdict(int), defaultdict(int), 0
         
-        for i in range(length - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
+        for num in nums:
+            if num > 0:
+                positives[num] += 1
+            elif num < 0:
+                negatives[num] += 1
+            elif zeros < 3:
+                zeros += 1
+        
+        if zeros >= 3:
+            out.append([0, 0, 0])
             
-            j = i + 1
-            k = length - 1
+        posNums = list(positives.keys())
+        negNums = list(negatives.keys())
+        
+        for i in range(len(posNums)):
+            num = posNums[i]
+            if -num in negatives and zeros > 0:
+                out.append([num, 0, -num])
             
-            while j < k:
-                total = nums[i] + nums[j] + nums[k]
-                if total == 0:
-                    out.append([nums[i], nums[j], nums[k]])
-                    j += 1
-                    k -= 1
-                    
-                    while j < length and nums[j] == nums[j - 1]:
-                        j += 1
-                    while k > 0 and nums[k] == nums[k + 1]:
-                        k -= 1
-                elif total > 0:
-                    k -= 1
-                    while k > 0 and nums[k] == nums[k + 1]:
-                        k -= 1
-                else:
-                    j += 1
-                    while j < length and nums[j] == nums[j - 1]:
-                        j += 1
+            if positives[num] > 1 and num * -2 in negatives:
+                out.append([num, num, num * -2])
+            
+            for j in range(i + 1, len(posNums)):
+                num2 = posNums[j]
+                negNum = -(num + num2)
+                if negNum in negatives:
+                    out.append([num, num2, negNum])
+                
+        for i in range(len(negNums)):
+            num = negNums[i]
+            
+            if negatives[num] > 1 and num * -2 in positives:
+                out.append([num, num, num * -2])
+            
+            for j in range(i + 1, len(negNums)):
+                num2 = negNums[j]
+                posNum = -(num + num2)
+                if posNum in positives:
+                    out.append([num, num2, posNum])
         
         return out
+            
+            
