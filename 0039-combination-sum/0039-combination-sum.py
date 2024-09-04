46e8b887e0e9b4dd0108811f1332bc5bee4750dc
Zheng Ze <zhengze@u.nus.edu>
2024-09-04 23:40:25 +0800

    Time: 59 ms (28.76%), Space: 16.7 MB (11.33%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        if target == 1:
+            return []
+        d = collections.defaultdict(int)
+        
+        for candidate in candidates:
+            if candidate > target:
+                continue
+            d[candidate] += 1
+        
+        out = []
+        def dfs(target, path, paths, floor):
+            if target < 0:
+                return
+            if target == 0:
+                paths.append(path)
+                return
+            for candidate in d:
+                if candidate < floor:
+                    continue
+                dfs(target - candidate, path + [candidate], paths, candidate)
+        
+        dfs(target, [], out, 2)
+        return out
+                
+                
