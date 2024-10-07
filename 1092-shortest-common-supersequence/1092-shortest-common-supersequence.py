ba11c9f71b86057ef79cbaed9e47b1ba82560e47
Zheng Ze <zhengze@u.nus.edu>
2024-10-07 16:55:15 +0800

    Time: 1025 ms (6.42%), Space: 490.7 MB (5.29%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
@@ -20,7 +20,7 @@ class Solution:
                     length, subsequence = dp[i - 1][j - 1]
                     dp[i][j] = (length + 1, subsequence + x[i - 1])
                 else:
-                    dp[i][j] = dp[i][j - 1] if dp[i][j - 1] >= dp[i - 1][j] else dp[i - 1][j]
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j], key=lambda x: x[0])
 
         # No LCS -> No way to reduce length -> just combine
         length, LCS = dp[len1][len2]
