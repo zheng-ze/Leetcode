8ab2ed67ef34917e64912059e23cd67004d9949a
Zheng Ze <zhengze@u.nus.edu>
2024-10-07 17:01:09 +0800

    Time: 772 ms (9.92%), Space: 410.7 MB (5.29%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
@@ -1,39 +1,37 @@
 class Solution:
+    
+    
+    
     def shortestCommonSupersequence(self, x: str, y: str) -> str:
-        len1, len2 = len(x), len(y)
+        def LCS(x: str, y: str) -> str:
+            len1, len2 = len(x), len(y)
 
-        if len1 == 0:
-            return y
-        if len2 == 0:
-            return x
+            if len1 == 0:
+                return y
+            if len2 == 0:
+                return x
 
-        # Find LCS first -> LCS will allow us to remove letters thereby shortening the string
-        dp = [["" for _ in range(len2 + 1)] for _ in range(len1 + 1)]
+            # Find LCS first -> LCS will allow us to remove letters thereby shortening the string
+            dp = [["" for _ in range(len2 + 1)] for _ in range(len1 + 1)]
 
-        for i in range(1, len1 + 1):
-            for j in range(1, len2 + 1):
-                if x[i - 1] == y[j - 1]:
-                    subsequence = dp[i - 1][j - 1]
-                    dp[i][j] = subsequence + x[i - 1]
-                else:
-                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j], key=len)
+            for i in range(1, len1 + 1):
+                for j in range(1, len2 + 1):
+                    if x[i - 1] == y[j - 1]:
+                        subsequence = dp[i - 1][j - 1]
+                        dp[i][j] = subsequence + x[i - 1]
+                    else:
+                        dp[i][j] = max(dp[i][j - 1], dp[i - 1][j], key=len)
 
-        # No LCS -> No way to reduce length -> just combine
-        LCS = dp[len1][len2]
-        if not LCS:
-            return x + y
-
-        # Same word
-        if len(LCS) == len1 == len2:
-            return x
-        
+            # No LCS -> No way to reduce length -> just combine
+            return dp[len1][len2]
+            
         out = ""
         i = j = 0
-        for c in LCS:
-            while i < len1 and x[i] != c:
+        for c in LCS(x, y):
+            while x[i] != c:
                 out += x[i]
                 i += 1
-            while j < len2 and y[j] != c:
+            while y[j] != c:
                 out += y[j]
                 j += 1
             
